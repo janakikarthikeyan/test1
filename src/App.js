bb549ef9a029d4ed24fafd6e04dca7bdc04966f6
janakikarthikeyan <61134129+janakikarthikeyan@users.noreply.github.com>
2020-02-20 11:04:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+
+
+
+
+    <header class="main-header">
+    <nav class="nav">
+    <a href="">Home</a>
+    <a href="">Gallery</a>
+    <a href="">Contact</a>
+    <a href="">About</a>
+    </nav>
+    </header>
+  );
+}
+
+export default App;
